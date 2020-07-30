daabea0808b36b7b93c8cef45959ae61594b2105
Mitchell Hillegeist <kwipso@gmail.com>
2020-07-29 20:12:07 -0700

    Loading all images from the start for easy use. Added card backs. Added temporary random card generator.

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-# import Image
 from PIL import Image, ImageTk
-# from tkinter import *
+import os
+from os import path
+import random as rand
 # 500x726,0.6887
 class Application(tk.Frame):
     """docstring for Application."""
@@ -14,24 +15,37 @@ class Application(tk.Frame):
         self.master.geometry("800x500")
         self.pack(fill="both", expand=1)
 
-        self.create_widgets()
+        self.images_load()
+        self.widgets_create()
 
-    def create_widgets(self):
+    def widgets_create(self):
         self.panel1 = tk.Frame(self)
         self.panel1.pack(side="left", fill="y")
 
         self.button1 = tk.Button(self.panel1)
         self.button1["text"] = "Press the button!"
+        self.button1["command"] = self.change_dealer_card
         self.button1.grid(row=1, column=1, sticky="e")
-
-        load = Image.open("sprites/red_joker.png")
-        load = load.resize((100, 145), Image.ANTIALIAS)
-        self.card_image = ImageTk.PhotoImage(load)
         #
-        self.card_dealer = tk.Label(self.panel1, image=self.card_image)
+        self.card_dealer = tk.Label(self.panel1, image=self.card_sprites["back"])
         # self.card_dealer["width"] = 100
         # self.card_dealer["width"] = 100
         self.card_dealer.grid(row=0, column=1)
+
+    def images_load(self):
+        self.card_sprites = dict()
+        files = os.listdir("sprites")
+
+        for file in files:
+            image = Image.open("sprites/" + file)
+            image = image.resize((100, 145), Image.ANTIALIAS)
+
+            self.card_sprites[file.replace(".png", "")] = ImageTk.PhotoImage(image)
+
+
+    def change_dealer_card(self):
+        self.card_dealer["image"] = rand.choice(list(self.card_sprites.values())) # self.card_sprites["10_of_clubs"]
+
 
 
 root = tk.Tk()
