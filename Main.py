661999bdf3ca371d633aed06149eb73f6802bb06
Mitchell Hillegeist <kwipso@gmail.com>
2020-07-30 21:19:14 -0700

    Improved layout.

--- Main.py
+++ Main.py
@@ -18,34 +18,54 @@ class Application(tk.Frame):
 
         self.game = Blackjack()
 
+        self.bg_color = "#006644"
+        self.card_width = 100
+        self.card_height = 145
+
         self.images_load()
         self.widgets_create()
 
     def widgets_create(self):
+        card_padx = 5
+        card_pady = 10
         self.panel1 = tk.Frame(self)
-        self.panel1["bg"] = "#006644"
-        self.panel1.pack(side="top", fill="y", expand=1)
+        self.panel1["bg"] = self.bg_color
+        self.panel1.pack(side="left", fill="both", expand=1)
 
-        self.button1 = tk.Button(self.panel1)
+        self.panel2 = tk.Frame(self)
+        self.panel2["bg"] = self.bg_color
+        self.panel2.pack(side="left", fill="y", expand=0)
+
+        self.panel3 = tk.Frame(self)
+        self.panel3["bg"] = self.bg_color
+        self.panel3.pack(side="left", fill="both", expand=1)
+
+        self.deck = tk.Label(self.panel2, image=self.card_sprites["back"])
+        # self.deck["visible"] = False
+        self.deck.grid(row=0, column=0, padx=20, pady=10)
+
+        self.house_hidden = tk.Label(self.panel2)
+        # self.house_hidden["width"] = self.card_width
+        self.house_hidden.grid(row=0, column=1, padx=card_padx, pady=card_pady)
+
+        self.house_vis1 = tk.Label(self.panel2)
+        self.house_vis1.grid(row=0, column=2, padx=card_padx, pady=card_pady)
+
+        self.panel_split = tk.Frame(self.panel2)
+        self.panel_split["height"] = 100
+        self.panel_split["bg"] = self.bg_color
+        self.panel_split.grid(row=1, column=0, columnspan=3)
+
+        self.player_c1 = tk.Label(self.panel2)
+        self.player_c1.grid(row=2, column=1, padx=card_padx, pady=card_pady)
+
+        self.player_c2 = tk.Label(self.panel2)
+        self.player_c2.grid(row=2, column=2, padx=card_padx, pady=card_pady)
+
+        self.button1 = tk.Button(self.panel2)
         self.button1["text"] = "Press the button!"
-        self.button1["command"] = self.change_dealer_card
-        self.button1.grid(row=1, column=1, sticky="e")
-
-        self.deck = tk.Label(self.panel1, image=self.card_sprites["back"])
-        self.deck["padx"] = 200
-        self.deck.grid(row=0, column=0)
-
-        self.house_hidden = tk.Label(self.panel1, image=self.card_sprites["back"])
-        self.house_hidden.grid(row=0, column=1)
-
-        self.house_vis1 = tk.Label(self.panel1, image=self.card_sprites["red_joker"])
-        self.house_vis1.grid(row=0, column=2)
-
-        self.player_c1 = tk.Label(self.panel1, image=self.card_sprites["red_joker"])
-        self.player_c1.grid(row=2, column=1)
-
-        self.player_c2 = tk.Label(self.panel1, image=self.card_sprites["red_joker"])
-        self.player_c2.grid(row=2, column=2)
+        self.button1["command"] = self.deal_cards
+        self.button1.grid(row=3, column=3, sticky="nsew")
 
 
     def images_load(self):
@@ -54,22 +74,24 @@ class Application(tk.Frame):
 
         for file in files:
             image = Image.open("sprites/" + file)
-            image = image.resize((100, 145), Image.ANTIALIAS)
+            image = image.resize((self.card_width, self.card_height), Image.ANTIALIAS)
 
             self.card_sprites[file.replace(".png", "")] = ImageTk.PhotoImage(image)
 
 
-    def change_dealer_card(self):
+    def deal_cards(self):
         # rand_num = rand.choice(range(0,52))
         # card_name = Blackjack.number_to_card(rand_num)
         # self.house_vis1["image"] = self.card_sprites[card_name]
         self.game.clear()
         self.game.deal()
 
+        house_card1 = "back"
         house_card2 = Blackjack.number_to_card( self.game.house[1] )
         player_card1 = Blackjack.number_to_card( self.game.player[0] )
         player_card2 = Blackjack.number_to_card( self.game.player[1] )
 
+        self.house_hidden["image"] = self.card_sprites[house_card1]
         self.house_vis1["image"] = self.card_sprites[house_card2]
         self.player_c1["image"] = self.card_sprites[player_card1]
         self.player_c2["image"] = self.card_sprites[player_card2]
