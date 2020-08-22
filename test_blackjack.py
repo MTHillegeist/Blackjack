32711cb8063bc71c8fb4894762f9301a6aa092e8
Mitchell Hillegeist <kwipso@gmail.com>
2020-08-21 19:56:50 -0700

    Renamed Main.py to main.py. Converted unit test module to use unittest.

--- test_blackjack.py
+++ test_blackjack.py
@@ -1,31 +1,28 @@
 import unittest
 from blackjack import Blackjack
 
-def hand_value_error(hand, correct, result):
-    print("Incorrect hand value {} found for hand:".format( result))
-    print(hand)
-    print("Correct Value: " + str(correct))
+class TestBlackjack(unittest.TestCase):
+    """docstring for TestBlackjack."""
 
-def hand_value_tests():
-    errors = 0
-    tests = [([9, 9], 20),
-             ([0, 9], 21),
-             ([0, 9, 9], 21),
-             ([0, 0, 9], 12),
-             ([0, 0, 0, 0, 5], 20),
-             ([13, 27, 51], 13)]#0, 1, 12 -> Ace, 2, King
+    def test_hand_value(self):
+        errors = 0
+        tests = [([9, 9], 20),
+                 ([0, 9], 21),
+                 ([0, 9, 9], 21),
+                 ([0, 0, 9], 12),
+                 ([0, 0, 0, 0, 5], 20),
+                 ([13, 27, 51], 13)]#0, 1, 12 -> Ace, 2, King
 
-    for test in tests:
-        hand = test[0]
-        correct = test[1]
-        result = Blackjack.hand_value(hand)
-        if(result != test[1]):
-            hand_value_error(test[0], test[1], result)
-            errors += 1
+        for test in tests:
+            hand = test[0]
+            correct = test[1]
+            result = Blackjack.hand_value(hand)
+            self.assertEqual(result, correct)
 
-    if(errors > 0):
-        print(str(errors) + " errors found.")
-    else:
-        print("No errors found.")
+    # def __init__(self, arg):
+    #     super(TestBlackjack, self).__init__()
+    #     self.arg = arg
 
-hand_value_tests()
+# hand_value_tests()
+if __name__ == '__main__':
+    unittest.main()
